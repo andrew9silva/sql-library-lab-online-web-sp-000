1cb207d46f95eee0f528d0db943500e2c142a926
Andrew Silva <andrewsilva9@gmail.com>
2019-09-30 03:14:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,13 +29,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.series_id) FROM series
-      INNER JOIN characters
-        ON series.id = characters.series_id
-      WHERE characters.species = 'human'
-      GROUP BY characters.series_id
-      ORDER BY characters.series_id DESC
-      LIMIT 1;"
+  ""
 end
 
 def select_character_names_and_number_of_books_they_are_in
