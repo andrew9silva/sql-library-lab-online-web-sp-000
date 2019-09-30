9a10730b94f4ce167e202a9942e5ede8ebc2e232
Andrew Silva <andrewsilva9@gmail.com>
2019-09-30 03:59:32 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,13 +29,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, COUNT(characters.series_id) FROM series
-      INNER JOIN characters
-        ON series.id = characters.series_id
-      WHERE characters.species = 'human'
-      GROUP BY characters.series_id
-      ORDER BY characters.series_id DESC
-      LIMIT 1;"
+"SELECT series.title, series.author_id, COUNT(characters.species) FROM series
+  INNER JOIN characters
+   ON series.author_id = characters.author_id
+    WHERE characters.species = 'human'
+    GROUP BY characters.species
+    ORDER BY characters.species DESC
+    LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
