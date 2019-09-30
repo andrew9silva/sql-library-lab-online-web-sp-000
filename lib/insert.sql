fef56451a85883cf3ea01b61af3e020a9b20c44f
Andrew Silva <andrewsilva9@gmail.com>
2019-09-30 02:37:36 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Series 1", 1, 1), (2, "Series 2", 2, 2);
+
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Title 1", 1998, 1), (2, "Title 2", 1999, 1),
+(3, "Title 3", 2000, 1), (4, "Title 4", 2005, 2), (5, "Title 5", 2007, 2), (6, "Title 6", 2009, 1);
